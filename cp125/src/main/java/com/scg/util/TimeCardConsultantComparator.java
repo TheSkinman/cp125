4ec97915b26cca3e36535e1684e7c840bac8796c
Norman Skinner <norman.skinner@live.com>
2019-01-22 23:17:47 -0800

    Almost done.

--- cp125/src/main/java/com/scg/util/TimeCardConsultantComparator.java
+++ cp125/src/main/java/com/scg/util/TimeCardConsultantComparator.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.scg.util;
 
 import java.util.Comparator;
@@ -8,15 +5,38 @@ import java.util.Comparator;
 import com.scg.domain.TimeCard;
 
 /**
+ * Compares two TimeCard objects ascending order by (in precedence order)
+ * consultant, time card starting date, total billable hours and non-billable
+ * hours.
+ * 
  * @author Norman Skinner (skinman@uw.edu)
  *
  */
 public class TimeCardConsultantComparator implements Comparator<TimeCard> {
 
+    /**
+     * Compares its two arguments, in ascending order by (in precedence order)
+     * consultant, the starting date, total billable hours and lastly total
+     * non-billable hours
+     * 
+     * @param firstTimeCard
+     *            the first object to be compared.
+     * @param secondTimeCard
+     *            the second object to be compared.
+     * @return A negative integer, zero, or a positive integer as the first argument
+     *         is less than, equal to, or greater than the second.
+     */
     @Override
     public int compare(TimeCard o1, TimeCard o2) {
-        // TODO Auto-generated method stub
-        return 0;
+        int diff = 0;
+        if (o1 != o2) {
+            if ((diff = o1.getConsultant().getName().getLastName().compareTo(o2.getConsultant().getName().getLastName())) == 0)
+            if ((diff = o1.getConsultant().getName().getFirstName().compareTo(o2.getConsultant().getName().getFirstName())) == 0)
+            if ((diff = o1.getConsultant().getName().getMiddleName().compareTo(o2.getConsultant().getName().getMiddleName())) == 0)
+            if ((diff = o1.getWeekStartingDay().compareTo(o2.getWeekStartingDay())) == 0)
+            if ((diff = Integer.compare(o1.getTotalBillableHours(), o2.getTotalBillableHours())) == 0)
+                diff = Integer.compare(o1.getTotalNonBillableHours(), o2.getTotalNonBillableHours());
+        }
+        return diff;
     }
-
 }
